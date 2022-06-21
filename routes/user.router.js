4318ad1c209c31758d6fb974f00342c5ce130555
Riddhiman <shubro17@outlook.com>
2022-06-21 17:41:20 +0530

    location3

--- routes/user.router.js
+++ routes/user.router.js
@@ -100,7 +100,7 @@ router.post('/login',(req,res,next)=>{
     })(req,res,next)
 })
 router.post('/location_details',async (req,res)=>{
-    console.log(req.body)
+    console.log(req.params)
     res.status(200).send({
         Message:'Location received'
     })
