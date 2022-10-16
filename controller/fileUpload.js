e88c999daa9698a4ae52f6f26141d4c613c6c055
Riddhiman <shubro17@outlook.com>
2022-10-16 20:01:49 +0530

    Changed amazon creds

--- controller/fileUpload.js
+++ controller/fileUpload.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 const s3 = new S3({
     accessKeyId: "AKIAVAR7LPOHXP7CV5AZ",
-    secretAccessKey: "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",
+    secretAccessKey: "ohFWM93ORpUoe4zGPy6HbMQb14uj/dsc+mUL/bIQ",
     region: "ap-south-1",
   });
 const uploadFile = (file)=>{
